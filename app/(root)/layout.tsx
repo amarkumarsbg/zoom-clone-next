52acc688783647acd32378c59f23c233dcdc2261
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-03-30 20:56:40 +0530

    Meeting room functionality completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,13 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
 import React, { ReactNode } from "react";
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
